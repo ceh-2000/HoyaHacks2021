a863f0a89ae5cf3ca055601345902df584712058
Clare Heinbaugh <clare.heinbaugh@gmail.com>
2021-01-30 15:25:10 -0500

    Added fetching and fixed javascript async request

--- vegan_friend/background.js
+++ vegan_friend/background.js
@@ -1,18 +1,26 @@
 // Regex-pattern to check URLs against. 
 // It matches URLs like: http[s]://[...]stackoverflow.com[...]
-var urlRegex = /^https?:\/\/(?:[^./?#]+\.)?stackoverflow\.com/;
+var urlRegex = /^https?:\/\/(?:[^./?#]+\.)?foodnetwork\.com/;
 
 // A function to use as callback
-function parseDom(domContent) {
-    alert('I received the following DOM content:\n' + domContent);
+async function parseDom(currentUrl) {
+    let parsedResponse = fetch('http://localhost:5000/meats/'+currentUrl, {method: 'GET'})
+                         .then(results => results.json())
+                         .then(console.log);
+    return parsedResponse
+}
+
+function showResponse(tabId, value){
+  chrome.tabs.sendMessage(tabId, {'method': value});
 }
 
 // When the browser-action button is clicked...
 chrome.webNavigation.onCompleted.addListener(function (details) {
     // ...check the URL of the active tab against our pattern and...
-
     if (urlRegex.test(details.url)) {
         // ...if it matches, send a message specifying a callback too
-        chrome.tabs.sendMessage(details.tabId, {text: 'report_back'}, parseDom);
-    }
+        let output = Promise.resolve(parseDom(details.url));
+        output.then(value => showResponse(details.tabId, value));
+
+      }
 });
--- vegan_friend/content.js
+++ vegan_friend/content.js
@@ -1,9 +1,24 @@
-// Listen for messages
-chrome.runtime.onMessage.addListener(function (msg, sendResponse) {
-    // If the received message has the expected format...
-    if (msg.text === 'report_back') {
-        // Call the specified callback, passing
-        // the web-page's DOM content as argument
-        sendResponse(document.all[0].outerHTML);
+chrome.runtime.onMessage.addListener(function(message,sender,sendResponse){
+    if(message.method == 'meat'){
+        flashBackground("red", "white");
+        document.title = "ARE YOU SURE YOU WANT TO EAT MEAT?"
+        document.body.innerHTML +='<div style="position:absolute;width:100%;height:100%;z-index:100;background:#000;"></div>';
+        return true;
     }
 });
+
+function flashBackground(color1, color2){
+    document.body.style.backgroundColor=color1
+    setTimeout(function(){ document.body.style.backgroundColor=color2;
+        setTimeout(function(){ document.body.style.backgroundColor=color1; 
+            setTimeout(function(){ document.body.style.backgroundColor=color2; 
+                setTimeout(function(){ document.body.style.backgroundColor=color1;
+                    setTimeout(function(){ document.body.style.backgroundColor=color2; 
+                        setTimeout(function(){ document.body.style.backgroundColor=color1; 
+                        }, 500);    
+                    }, 500);
+                }, 500);
+            }, 500); 
+        }, 500);
+    }, 500);
+}
